67788a3df5b6c91f6d62131f2ed88e74d4623088
gogeruk <gogeruk_locker@protonmail.com>
2022-07-06 14:42:46 +0300

    works? LOOKS LIKE IT!!!

--- app/config/web.php
+++ app/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Kwfskx40xpVDSjpjbrsCtYXGLKvhBMO9',
+            'cookieValidationKey' => '5FXIZxtrJF4_b65wrceAOay-Bwh9Q2rQ',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
